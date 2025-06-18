6251f612a60d7349ec108b453e20b57cc539dd28
Andreas Adam <andreas.adam@parcellab.com>
2025-06-18 14:58:28 +0200

    update package dependencies and refactor imports in UI components

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,4 +1,6 @@
-import type * as React from "react"
+"use client"
+
+import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
 
